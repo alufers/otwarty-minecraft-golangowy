1a01973286b882d93ce71111a87195994f086552
alufers <alufers@wp.pl>
2018-12-29 18:45:47 +0100

    Finished shader program

--- omg/shader_program.go
+++ omg/shader_program.go
@@ -21,39 +21,17 @@ func newShaderProgram(vertexSource, fragmentSource string) *shaderProgram {
 }
 
 func (sp *shaderProgram) compile() error {
-	sp.vertexShader = gl.CreateShader(gl.VERTEX_SHADER)
-	vertexSourceStr, free := gl.Strs(sp.vertexSource)
-	gl.ShaderSource(sp.vertexShader, 1, vertexSourceStr, nil)
-	free()
-	gl.CompileShader(sp.vertexShader)
-	var status int32
-	gl.GetShaderiv(sp.vertexShader, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(sp.vertexShader, gl.INFO_LOG_LENGTH, &logLength)
+	var err error
+	sp.vertexShader, err = sp.compileSingleShader(sp.vertexSource, gl.VERTEX_SHADER)
 
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(sp.vertexShader, logLength, nil, gl.Str(log))
-
-		return fmt.Errorf("failed to compile vertex shader: %v", log)
+	if err != nil {
+		return err
 	}
+	sp.fragmentShader, err = sp.compileSingleShader(sp.fragmentSource, gl.FRAGMENT_SHADER)
 
-	sp.fragmentShader = gl.CreateShader(gl.FRAGMENT_SHADER)
-	fragmentSourceStr, free := gl.Strs(sp.fragmentSource + "\x00")
-	gl.ShaderSource(sp.fragmentShader, 1, fragmentSourceStr, nil)
-	free()
-	gl.CompileShader(sp.fragmentShader)
-	gl.GetShaderiv(sp.fragmentShader, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(sp.fragmentShader, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(sp.fragmentShader, logLength, nil, gl.Str(log))
-
-		return fmt.Errorf("failed to compile fragment shader: %v", log)
+	if err != nil {
+		return err
 	}
-
 	sp.program = gl.CreateProgram()
 	gl.AttachShader(sp.program, sp.vertexShader)
 	gl.AttachShader(sp.program, sp.fragmentShader)
@@ -61,6 +39,27 @@ func (sp *shaderProgram) compile() error {
 	gl.DeleteShader(sp.fragmentShader)
 	gl.DeleteShader(sp.vertexShader)
 	return nil
+}
+
+func (sp *shaderProgram) compileSingleShader(source string, shaderType uint32) (uint32, error) {
+	sh := gl.CreateShader(shaderType)
+	fragmentSourceStr, free := gl.Strs(source + "\x00")
+	gl.ShaderSource(sh, 1, fragmentSourceStr, nil)
+	free()
+	gl.CompileShader(sh)
+	var status int32
+	gl.GetShaderiv(sh, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(sh, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(sh, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile fragment shader: %v", log)
+	}
+
+	return sh, nil
 }
 
 func (sp *shaderProgram) use() {
